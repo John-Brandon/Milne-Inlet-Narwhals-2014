6f4b5e7a0b61aa32d66dde87dc11c4b562c9462b
John Brandon <jbrandon@gmail.com>
2014-10-23 10:21:58 -0700

    Code to start making data tables with 2014 data

---
+++ MilneNarwhal-Table2014Data.R
@@ -0,0 +1,20 @@
+###==============
+# Author: John R. Brandon
+# eMail:  jbrandon greeneridge [or jbrandon@gmail.com]
+# Date :  Fall 2014
+# OS   :  Mac OS 10.9.5 (x86_64-apple-darwin10.8.0 (64-bit))
+# Language : R (ver 3.0.2 (2013-09-25) -- "Frisbee Sailing")
+# IDE  : Rstudio v 0.98.484
+###=== === +++ === === +++ === === +++ === ===
+# Purpose : Code to table Milne Inlet narwhal data for LGL / Baffinland 
+#  Notes : These routines work on data that has already been extracted and munged, so the first step is to load that pre-massaged data 
+#  1) Your main directory will differ. See 'base.dir' variable below to edit that path to match your system
+#  2) POSIXlt is a class for representing calendar dates and times (in decreasing order to be unambiguous, i.e. "YYYY-MM-DD HH:MM:SS")
+#  3) These data are also known as "Relative Abundance and Distribution" (RAD) data 
+#
+#====== +++ === === +++ === === +++ === ===
+library(plyr) # Hadley Wickham's "Plier" package for common tasks (e.g. summarizing) with data.frames
+
+load("~/Documents/2014 Work/Milne Inlet Narwhals/2014 Analysis/Code/MilneNarwhal.2014.RData") # Load workspace 
+setwd("~/Documents/2014 Work/Milne Inlet Narwhals/Data/2014") # Set working directory for data
+
