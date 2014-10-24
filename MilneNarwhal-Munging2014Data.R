2680c3f93786417c973adae534113b23bdfaac82
John Brandon <jbrandon@gmail.com>
2014-10-23 17:54:19 -0700

    Some more munging and tabling in this commit

--- MilneNarwhal-Munging2014Data.R
+++ MilneNarwhal-Munging2014Data.R
@@ -9,16 +9,13 @@
 # Purpose : Visualize and analyze Milen Inlet narwhal data for LGL / Baffinland 
 #  Notes : 
 #  1) Your main directory will differ. See 'base.dir' variable below to edit that path to match your system
-#  2) POSIXlt is a class for representing calendar dates and times (in decreasing order to be unambiguous, i.e. "YYYY-MM-DD HH:MM:SS")
+#  2) POSIXct is a class for representing calendar dates and times (these must be input in a strictly decreasing unambigous order, i.e. "YYYY-MM-DD HH:MM:SS")
 #  3) These data are also known as "Relative Abundance and Distribution" (RAD) data 
 #
 #====== +++ === === +++ === === +++ === ===
 if (getOption("stringsAsFactors")) options(stringsAsFactors = FALSE) # set global option, don't want strings read as factors
 getOption("stringsAsFactors") # check
 
-#library(reshape2) # handy functions for data munging, via Hadley Wickham
-#detach("package:reshape2", unload=TRUE)
-#library(reshape)
 library(plyr) # Hadley Wickham's "Plier" package for common tasks (e.g. summarizing) with data.frames
 
 rm(list=ls()) # clear leftovers from previous workspace
@@ -26,8 +23,8 @@ rm(list=ls()) # clear leftovers from previous workspace
 load("~/Documents/2014 Work/Milne Inlet Narwhals/2014 Analysis/Code/MilneNarwhal.2014.RData") # Load workspace 
 
 setwd("~/Documents/2014 Work/Milne Inlet Narwhals/Data/2014") # Set working directory for data
-dfile = "2014.milne.inlet.narwhal.csv"
-dat2014 = read.csv(file = dfile, header = TRUE, as.is = TRUE, na.strings = c("NA", "x", "X"))
+dfile = "2014.milne.inlet.narwhal.csv" # 2014 RAD data file, saved as comma delimited
+dat2014 = read.csv(file = dfile, header = TRUE, as.is = TRUE, na.strings = c("NA", "x", "X")) # Read data file 
 
 #====== +++ === === +++ === === +++ === ===
 # Clean up some typos
@@ -43,30 +40,24 @@ dat2014$Stratum = substring(dat2014$SubStratum, first = 1, last = 1) # Create a 
 dat2014$SubStratum.num = substring(dat2014$SubStratum, first = 2, last = 2) # Create a vector with SubStratum.num from SubStratum vector
 
 dat2014$datetime = with(dat2014, paste(Date, Time)) # Convert DateTime to POSIXlt class
-dat2014$datetime = as.POSIXlt(dat2014$datetime) # not really necessary at this stage, but a place holder for possible future analysis / visualizing time series
+dat2014$datetime = as.POSIXct(dat2014$datetime) # not really necessary at this stage, but a place holder for possible future analysis / visualizing time series
 
-#====== +++ === === +++ === === +++ === ===
-# Keep munging
-#====== +++ === === +++ === === +++ === ===
 length(unique(dat2014$datetime)) # number of "Relative Abundance and Distribution" (RAD) samples in 2014
 
 #====== +++ === === +++ === === +++ === ===
-# Start summarizing counts by making tables
-# ?table
+# Keep munging:
+# Table counts of group sizes by sub-stratum
 #====== +++ === === +++ === === +++ === ===
-table(dat2014$Stratum, dat2014$GroupSize)
-as.data.frame(table(dat2014$Stratum, dat2014$GroupSize))
-group.size = table(dat2014$SubStratum, dat2014$GroupSize)
-group.size = as.data.frame(group.size)
-names(group.size) = c("SubStratum", "GroupSize", "Freq")
-write.csv(file = "foo.csv", x = group.size); system("open foo.csv") # check
+group.size = table(dat2014$SubStratum, dat2014$GroupSize) # ?table
+group.size = as.data.frame(group.size) # data.frame with group size frequencies in counts (e.g. 1 group of 34 in substratum F1, 0 groups of 33, etc.)
+names(group.size) = c("SubStratum", "GroupSize", "Freq") # make it easier to read
+write.csv(file = "foo.csv", x = group.size, row.names = FALSE); system("open foo.csv") # check
 
 #====== +++ === === +++ === === +++ === ===
 # Summarize abundance by SubStratum (integrating over time)
-# ?table
 #====== +++ === === +++ === === +++ === ===
 tot.counts = group.size
-i = sapply(tot.counts, is.factor) # intermediate step to convert from factor to character
+i = sapply(tot.counts, is.factor) # intermediate step to convert columns that are presently factors to characters
 tot.counts[i] <- lapply(tot.counts[i], as.character) # convert columns that are factors to character strings
 tot.counts$TotalCount = as.numeric(tot.counts$GroupSize) * tot.counts$Freq # TotalCounts are product of group size and numbers of groups
 tot.counts$Stratum = substring(tot.counts$SubStratum, first = 1, last = 1) # Create a vector with Stratum from SubStratum vector
@@ -79,11 +70,16 @@ head(tot.counts.subs)
 write.csv(file = "foo.csv", x = foo); system("open foo.csv") # check
 
 #====== +++ === === +++ === === +++ === ===
-# NEXT STEP: Summarize by Stratum (integrating over time)
+# Summarize by Stratum (integrating over time)
 #====== +++ === === +++ === === +++ === ===
 tot.counts.strat = ddply(tot.counts, "Stratum", summarise, TotalCount = sum(TotalCount)) # uses 'plyr' package, could also use function aggregate
-head(tot.counts.strat)
+head(tot.counts.strat) # check
 tot.counts.strat
+
 library(ggplot2)
 ggplot(tot.counts.strat, aes(x = Stratum, y = TotalCount)) + geom_bar(stat = "identity")
 
+#====== +++ === === +++ === === +++ === ===
+# Save workspace image
+#====== +++ === === +++ === === +++ === ===
+save.image("~/Documents/2014 Work/Milne Inlet Narwhals/2014 Analysis/Code/MilneNarwhal.2014.RData")
--- MilneNarwhal-Table2014Data.R
+++ MilneNarwhal-Table2014Data.R
@@ -9,12 +9,112 @@
 # Purpose : Code to table Milne Inlet narwhal data for LGL / Baffinland 
 #  Notes : These routines work on data that has already been extracted and munged, so the first step is to load that pre-massaged data 
 #  1) Your main directory will differ. See 'base.dir' variable below to edit that path to match your system
-#  2) POSIXlt is a class for representing calendar dates and times (in decreasing order to be unambiguous, i.e. "YYYY-MM-DD HH:MM:SS")
+#  2) 
 #  3) These data are also known as "Relative Abundance and Distribution" (RAD) data 
-#
+#  4) Loads existing workspace, that contains 2014 data (dat2014)
 #====== +++ === === +++ === === +++ === ===
 library(plyr) # Hadley Wickham's "Plier" package for common tasks (e.g. summarizing) with data.frames
 
 load("~/Documents/2014 Work/Milne Inlet Narwhals/2014 Analysis/Code/MilneNarwhal.2014.RData") # Load workspace 
 setwd("~/Documents/2014 Work/Milne Inlet Narwhals/Data/2014") # Set working directory for data
 
+write.csv(x = dat2014, file = "foo.csv"); system("open foo.csv") # check
+
+#====== +++ === === +++ === === +++ === ===
+# Table X. Abundance data effort at Bruce Head in 2014, 
+#  including the number of counts associated with the presence of large vessels. 
+#  Includes counts made in all sighting conditions (poor, good and excellent)
+#  Columns = Date, Start Time, End Time, Number of counts, Counts during vessel presence 
+#====== +++ === === +++ === === +++ === ===
+
+# For each unique day, get the start (earliest) and end (latest) time for counts, and sum the total number of counts
+foo.table = ddply(dat2014, "Date", summarise, StartTime = substr(min(datetime), 12, 16), EndTime = substr(max(datetime), 12, 16), 
+            Counts = length(unique(Time))) # , Counts.With.Vessel = would be nice to add this but do by hand for now
+
+AddVesselCounts = function(dat){ # horrible naming convention here, apologies
+  foo = NULL; goo = NULL
+  foo = unique(dat)
+  goo = which(foo == "PRE" | foo == "C" | foo == "POST")
+  goo = length(goo)
+  return(goo)
+}
+
+foo.table = ddply(dat2014, "Date", summarise, StartTime = substr(min(datetime), 12, 16), EndTime = substr(max(datetime), 12, 16), 
+                  Counts = length(unique(Time)), Counts.With.Vessel = AddVesselCounts(CountType)) 
+
+write.csv(x = foo.table, file = "foo.csv", row.names = FALSE); system("open foo.csv") # check
+
+#====== +++ === === +++ === === +++ === ===
+# Table XX. Number of narwhals (individuals) per stratum, number of abundance counts, and
+# mean number of narwhals per count considering all abundance data.
+#====== +++ === === +++ === === +++ === ===
+foo.table2 = ddply(dat2014, "Date", summarise, 
+                   A = sum(GroupSize[which(Stratum=="A")], na.rm = TRUE), # this is pretty cluggy
+                   B = sum(GroupSize[which(Stratum=="B")], na.rm = TRUE),
+                   C = sum(GroupSize[which(Stratum=="C")], na.rm = TRUE),
+                   D = sum(GroupSize[which(Stratum=="D")], na.rm = TRUE),
+                   E = sum(GroupSize[which(Stratum=="E")], na.rm = TRUE),
+                   F = sum(GroupSize[which(Stratum=="F")], na.rm = TRUE),
+                   G = sum(GroupSize[which(Stratum=="G")], na.rm = TRUE),
+                   H = sum(GroupSize[which(Stratum=="H")], na.rm = TRUE),
+                   I = sum(GroupSize[which(Stratum=="I")], na.rm = TRUE)) # , Counts.With.Vessel = would be nice to add this but do by hand for now
+
+write.csv(x = foo.table2, file = "foo.csv", row.names = FALSE); system("open foo.csv") # check
+
+#====== +++ === === +++ === === +++ === ===
+# Table XX. Number of narwhals per stratum, number of abundance counts, and mean number of
+#  narwhals per count during periods of good or excellent sightability conditions in August 2013.
+#====== +++ === === +++ === === +++ === ===
+
+# make a column which assigns an ID number for each count (a single day may have multiple counts)
+count.id = seq(from = 1, to = length(unique(dat2014$datetime))); count.id
+ii = NULL
+dat2014$Count.id = rep(-99, nrow(dat2014))
+for(ii in 1:length(unique(dat2014$datetime))){ # probably a more elegant way to do this, rather than a loop.
+  rec.numbers = NULL
+  rec.numbers = which(dat2014$datetime == unique(dat2014$datetime)[ii])  
+  dat2014$Count.id[rec.numbers] = count.id[ii]
+}
+foo = dat2014
+write.csv(x = foo, file = "foo.csv", row.names = FALSE); system("open foo.csv") # check
+
+# Designate counts for Inclusion, if they were conducted during complete good to excellent sightability 
+#  Any poor sightability, even if just one substratum, results in exclusion of that entire count at present
+#  Note also, that instead of "P" for poor, if no attempt at a count was made for a sub-stratum (e.g. due to fog)
+#    then those were entered as "x" (read into R as "NA"), so those will need to be excluded as well under this criteria
+unique(dat2014$Sightability)
+length(which(is.na(dat2014$Sightability)))
+length(which(dat2014$Sightability == "P"))
+length(which(dat2014$Sightability == "L"))
+length(which(dat2014$Sightability == 3)) 
+
+unique(dat2014$Count.id[which(is.na(dat2014$Sightability))]) # check to see which count.id's had NA's
+unique(dat2014$Count.id[which(dat2014$Sightability == "P")]) # check to see which count.id's had P's
+foo = subset(dat2014, Count.id == 28)
+dim(foo)
+
+CountInclude = function(sightability){
+  # Does this count meet the criteria of having all sub-stratum observed during Good or Excellent conditions?
+  # 'sightability' here is a vector containing a code ("P", "G", "E", or NA) for each sub-stratum in a given count 
+  # 'include.count' is returned as TRUE or FALSE
+  unique.sight.codes = NULL; include.count = TRUE
+  unique.sight.codes = unique(sightability)
+  if ("P" %in% unique(sightability)) include.count = FALSE
+  if (NA %in% unique(sightability)) include.count = FALSE
+  return(include.count)
+}
+
+counts.to.include = ddply(dat2014, "Count.id", summarise, Include.count = CountInclude(Sightability)) # ! means NOT in R, so if no "P" then Include = TRUE 
+write.csv(x = counts.to.include, file = "foo.csv", row.names = FALSE); system("open foo.csv") # check
+
+dat2014$IncludeCount = rep(FALSE, nrow(dat2014))
+
+for(ii in 1:nrow(counts.to.include)){
+  dat2014$IncludeCount[which(dat2014$Count.id == counts.to.include[ii,1])] = counts.to.include[ii,2]
+}
+write.csv(x = dat2014, file = "foo.csv", row.names = FALSE); system("open foo.csv") # check
+
+# subset data to exclude abundance counts that included at least one strata with poor sightability
+dat2014.keepers = subset(dat2014, IncludeCount)
+unique(dat2014.keepers$IncludeCount)
+dim(dat2014.keepers); dim(dat2014)
